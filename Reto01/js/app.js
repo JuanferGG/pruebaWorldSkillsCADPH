a56fe4c9ec18706d69a4ddc36eb7d6c6e5b03084
Juan Fernando <juanfe15777@gmail.com>
2023-05-17 14:56:41 -0500

    WordSkills - Version Final LandingPage

--- Reto01/js/app.js
+++ Reto01/js/app.js
@@ -1,1 +1,26 @@
-console.log('Hola mundo')
+const btn_left = document.getElementById('btn-l')
+const btn_right = document.getElementById('btn-r')
+
+const imgs = ["../imgs/img02.jpg","../imgs/img03.jpg","../imgs/img04.jpg"]
+
+const contImgs = document.getElementById('imgs')
+let cout = 1
+
+btn_right.addEventListener('click', () => {
+    if(cout >= imgs.length){
+        cout = 0
+        contImgs.style.backgroundImage = `url(${imgs[cout]})`
+    } else {
+        contImgs.style.backgroundImage = `url(${imgs[cout]})`
+        cout++
+    }
+})
+btn_left.addEventListener('click', () => {
+    if(cout < 0){
+        cout = imgs.length - 1
+        contImgs.style.backgroundImage = `url(${imgs[cout]})`
+    } else {
+        contImgs.style.backgroundImage = `url(${imgs[cout]})`
+        cout--
+    }
+})
